19e09d5a2cbe2bb529d1f064df1adfff925fbf50
adyrathore <adyrathore@gmail.com>
2017-09-02 22:58:01 +0530

    2 Sep Aditya

--- app/src/main/java/com/app/hakeem/fragment/FragmentSplash.java
+++ app/src/main/java/com/app/hakeem/fragment/FragmentSplash.java
@@ -25,6 +25,7 @@ public class FragmentSplash extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false);
+
     }
 
 }
