59fd07d958eaf7c6bb07d54466ebb588daa46f06
aditya.singh <adyrathore@gmail.com>
2018-01-23 12:33:04 +0530

    Ady Commit 1st time

--- app/src/main/java/com/app/hakeem/ActivityContainer.java
+++ app/src/main/java/com/app/hakeem/ActivityContainer.java
@@ -1,14 +1,101 @@
 package com.app.hakeem;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
-public class ActivityContainer extends Activity {
+import com.app.hakeem.fragment.FragmentSplash;
+import com.app.hakeem.util.C;
+
+public class ActivityContainer extends AppCompatActivity {
+
+    private Fragment fragment;
+    private TextView tvTitle;
+    private Bundle bundle;
+    private int fragmentAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        bundle = getIntent().getBundleExtra(C.BUNDLE);
+        fragmentAction = getIntent().getIntExtra(C.FRAGMENT_ACTION, C.FRAGMENT_SPLASH);
+        fragmnetLoader(fragmentAction, bundle);
+
     }
+
+    public void fragmnetLoader(int fragmentType, Bundle bundle) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        switch (fragmentType) {
+            case C.FRAGMENT_SPLASH:
+                getSupportActionBar().hide();
+                fragment = new FragmentSplash();
+                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.addToBackStack(C.TAG_FRAGMENT_SPLASH);
+                break;
+
+        }
+        fragment.setArguments(bundle);
+
+        fragmentTransaction.commit();
+        getSupportFragmentManager().executePendingTransactions();
+
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+
+        getSupportFragmentManager().executePendingTransactions();
+        if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
+
+//            Log.e("CountPop", getSupportFragmentManager().getBackStackEntryCount() + "");
+
+            int fragmentCount = getSupportFragmentManager().getBackStackEntryCount();
+            FragmentManager.BackStackEntry backEntry = getSupportFragmentManager().getBackStackEntryAt(fragmentCount - 2);
+            String fragmentTag = backEntry.getName();
+
+            getSupportFragmentManager().popBackStack();
+
+            getSupportFragmentManager().executePendingTransactions();
+            if (fragmentTag != null) {
+                switch (fragmentTag) {
+//                    case C.TAG_FRAGMENT_BOOK_A_FLIGHT:
+//                        tvTitle.setText(C.TAG_FRAGMENT_BOOK_A_FLIGHT);
+////                        tvSubTitle.setVisibility(View.GONE);
+////                        tvTitle2.setVisibility(View.GONE);
+//                        break;
+//                    case C.TAG_FRAGMENT_SEARCH_FLIGHT:
+//                        tvTitle.setText(C.TAG_FRAGMENT_SEARCH_FLIGHT);
+//                        break;
+//                    case C.TAG_FRAGMENT_OURDESTINATIONS:
+//                        tvTitle.setText(getResources().getString(R.string.our_destinations));
+//                        break;
+//                    case C.TAG_FRAGMENT_MY_ACCOUNT:
+//                        tvTitle.setText(getResources().getString(R.string.my_account));
+//                        break;
+                }
+            }
+
+
+        } else {
+            finish();
+        }
+    }
+
 
 }
---
+++ app/src/main/java/com/app/hakeem/util/C.java
@@ -0,0 +1,13 @@
+package com.app.hakeem.util;
+
+/**
+ * Created by aditya.singh on 1/23/2018.
+ */
+
+public interface C {
+
+    int FRAGMENT_SPLASH =1;
+    String TAG_FRAGMENT_SPLASH ="fragment_splash" ;
+    String BUNDLE = "bundle";
+    String FRAGMENT_ACTION ="action" ;
+}
