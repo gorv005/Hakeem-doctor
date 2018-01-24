99911655945cdeba29c5fb6a9367f56594f0537d
aditya.singh <adyrathore@gmail.com>
2018-01-24 16:43:47 +0530

    Ady Commit 1st time

---
+++ app/src/main/java/com/app/hakeem/fragment/FragmentPatientRegistrationStep1.java
@@ -0,0 +1,30 @@
+package com.app.hakeem.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.hakeem.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentPatientRegistrationStep1 extends Fragment {
+
+
+    public FragmentPatientRegistrationStep1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_patient_registration_step1, container, false);
+    }
+
+}
