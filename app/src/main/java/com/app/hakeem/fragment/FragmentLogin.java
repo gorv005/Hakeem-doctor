b129542bf42b9cd9118882852a9a695dd66506a2
aditya.singh <adyrathore@gmail.com>
2018-01-24 12:44:01 +0530

    Ady Commit 1st time

--- app/src/main/java/com/app/hakeem/fragment/FragmentLogin.java
+++ app/src/main/java/com/app/hakeem/fragment/FragmentLogin.java
@@ -2,29 +2,54 @@ package com.app.hakeem.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.app.hakeem.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentLogin extends Fragment {
 
+    @BindView(R.id.etUserName)
+    EditText tvUserName;
+    @BindView(R.id.etPassword)
+    EditText tvPassword;
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
+    @BindView(R.id.btnCreateAccount)
+    Button btnCreateAccount;
+    @BindView(R.id.btnNeedHelp)
+    Button btnNeedHelp;
+
 
     public FragmentLogin() {
-        // Required empty public constructor
+
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+
+
+
+    }
 }
