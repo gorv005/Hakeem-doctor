5de44dfd2fe8ab43d3e6de11a50fb3794d3ea6f4
aditya.singh <adyrathore@gmail.com>
2018-01-24 13:09:04 +0530

    Ady Commit 1st time

--- app/src/main/java/com/app/hakeem/fragment/FragmentLogin.java
+++ app/src/main/java/com/app/hakeem/fragment/FragmentLogin.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.app.hakeem.R;
+import com.app.hakeem.util.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,12 +19,12 @@ import butterknife.ButterKnife;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentLogin extends Fragment {
+public class FragmentLogin extends Fragment implements View.OnClickListener {
 
     @BindView(R.id.etUserName)
-    EditText tvUserName;
+    EditText etUserName;
     @BindView(R.id.etPassword)
-    EditText tvPassword;
+    EditText etPassword;
     @BindView(R.id.btnLogin)
     Button btnLogin;
     @BindView(R.id.btnCreateAccount)
@@ -50,6 +51,46 @@ public class FragmentLogin extends Fragment {
         ButterKnife.bind(this, view);
 
 
+        btnLogin.setOnClickListener(this);
+        btnCreateAccount.setOnClickListener(this);
+        btnNeedHelp.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btnLogin:
+
+                if(isAllValid())
+                {
+//                    doLogin();
+                }
+
+                break;
+            case R.id.btnNeedHelp:
+                break;
+            case R.id.btnCreateAccount:
+                break;
+
+        }
+    }
+
+    private boolean isAllValid() {
+
+
+        if(!Util.isValidMail(etUserName.getText().toString()))
+        {
+            etUserName.setError(getActivity().getResources().getString(R.string.enter_valid_mail));
+            return false;
+        }
+        else if(etPassword.getText().toString().length()==0)
+        {
+            etPassword.setError(getActivity().getResources().getString(R.string.enter_password));
+            return false;
+        }
+
+        return true;
     }
 }
